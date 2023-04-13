8cf80c34e021617ca882b03ef2e38a2c59774842
Kiran Gaikwad <tejaswinikale165@gmail.com>
2023-04-13 16:27:48 +0200

    Remove logs

--- src/main/java/com/project/webservice/crs/controller/CustomerController.java
+++ src/main/java/com/project/webservice/crs/controller/CustomerController.java
@@ -1,15 +1,15 @@
 package com.project.webservice.crs.controller;
 import com.project.webservice.crs.model.CustomerReg;
 import com.project.webservice.crs.services.CustomerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
-    private static final Logger LOGGER= LoggerFactory.getLogger(CustomerController.class);
+    //private static final Logger LOGGER= LoggerFactory.getLogger(CustomerController.class);
     @Autowired
     CustomerService customerService;
 
