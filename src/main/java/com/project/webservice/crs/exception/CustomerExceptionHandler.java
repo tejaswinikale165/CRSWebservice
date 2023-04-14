a123ed3d66f87b1b00eb6504e690c00ebb562654
Kiran Gaikwad <tejaswinikale165@gmail.com>
2023-04-14 14:30:46 +0200

    add exception hadding for bad request and not found

---
+++ src/main/java/com/project/webservice/crs/exception/CustomerExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.project.webservice.crs.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomerExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Object> handleException(Exception ex, WebRequest request){
+        HttpStatus status=HttpStatus.INTERNAL_SERVER_ERROR;
+        String message="Problem occured during handling your request";
+        if(ex instanceof BadRequestException)
+        {
+            status=HttpStatus.BAD_REQUEST;
+            message = ex.getMessage();
+        }
+
+        else if(ex instanceof NotFoundException)
+        {
+            status=HttpStatus.NOT_FOUND;
+            message = ex.getMessage();
+        }
+
+        ErrorResponse errorResponse=new ErrorResponse(status,message);
+return new ResponseEntity<>(errorResponse, status);
+    }
+}
---
+++ src/main/java/com/project/webservice/crs/exception/ErrorResponse.java
@@ -0,0 +1,30 @@
+package com.project.webservice.crs.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+
+    HttpStatus status;
+    String message;
+
+    public ErrorResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
