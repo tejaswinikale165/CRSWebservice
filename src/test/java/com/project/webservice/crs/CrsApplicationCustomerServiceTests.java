9cb8a4af68e430bd1d4df3df07f05cab11ff4584
Kiran Gaikwad <tejaswinikale165@gmail.com>
2023-04-17 16:43:02 +0200

    upadte package name of service

--- src/test/java/com/project/webservice/crs/CrsApplicationCustomerServiceTests.java
+++ src/test/java/com/project/webservice/crs/CrsApplicationCustomerServiceTests.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional
-public class CustomerServiceTests {
+public class CrsApplicationCustomerServiceTests {
     @MockBean
     CustomerRepoImpl customerRepo;
 
